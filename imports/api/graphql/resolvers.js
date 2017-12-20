de7e34361b729cece67f5e8e16f8624aff8cb457
Simon <simon@J.A.R.V.I.S>
2017-12-20 19:05:41 +0100

    redefinit le chemin vers json pour production

--- imports/api/graphql/resolvers.js
+++ imports/api/graphql/resolvers.js
@@ -10,7 +10,7 @@ const resolvers = {
       /*filter parcoure le tableau donné en premier argument (ici le contenu du fichier json, plus tard la réponse d'une api sécurisée),
       le deuxième argument est une fonction qui recoit l'objet intitulé patient correspondant au patient individuel trouvé dans le tableau du premier argument,
       si la fonction return, elle push un nouveau tableau avec le patient qui a passé le test de correspondance avec les id ciblés*/
-      return _.filter(HTTP.get(Meteor.absoluteUrl("/MOCK_DATA.JSON")).data, (patient)=>{
+      return _.filter(HTTP.get(Meteor.absoluteUrl("MOCK_DATA.JSON")).data, (patient)=>{
         /*si un des id dans targetedPatientsIds correspond à l'id du patient*/
         if (_.some(targetedPatientsIds, (id)=>(id === patient.id))){
           /*ajoute ce patient au tableau renvoyé au client*/
@@ -20,7 +20,7 @@ const resolvers = {
     },
     /*Renvoie uniquement les noms et id des patients pour permettre de les cibler*/
     patientsToTarget: ()=>{
-      return HTTP.get(Meteor.absoluteUrl("/MOCK_DATA.JSON")).data.map((patient, index)=>(
+      return HTTP.get(Meteor.absoluteUrl("MOCK_DATA.JSON")).data.map((patient, index)=>(
         {
           name: `${patient.admin.prenom} ${patient.admin.nom}`,
           id: patient.id,
